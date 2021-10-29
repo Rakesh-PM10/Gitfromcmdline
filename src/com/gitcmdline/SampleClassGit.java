293f36e36e3e88240d3fde13d201fef6da58592a
Rakesh-PM10 <rakeshpm7@gmail.com>
2021-10-29 13:09:25 +0530

    branch1 commit 1

--- src/com/gitcmdline/SampleClassGit.java
+++ src/com/gitcmdline/SampleClassGit.java
@@ -3,6 +3,7 @@ package com.gitcmdline;
 public class SampleClassGit {
 	public static void main(String[] args) {
 		System.out.println("My first changes");
+		System.out.println("code for Branch1");
 	}
 
 }
